0f687d885ee52c775abe1d8a863534ae2750f914
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-05 13:54:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ return object
 }
 
 function deleteFromObjectByKey(object, key) {
-  Object.assign({}, object)
-  delete object.key
-  return object
+  newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
 }
