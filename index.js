7c5f4c44fbf969fff4d79d5aee5db842ac1d2557
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-05 13:46:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
- Object.assign({}, object, {key:'value'})
- return Object
+ return Object.assign({}, object, {key:'value'})
+
 }
